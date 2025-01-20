051e3c118177383aa54f5d8df3ac9fc3676eaf2c
Ilya Prokofev <87380526+ilyaprofev@users.noreply.github.com>
2025-01-20 13:14:54 +0300

    new data calss

--- composeApp/src/androidMain/kotlin/com/ilya/meetmapkmp/SocialMap/ViewModel/ChatViewModel.kt
+++ composeApp/src/androidMain/kotlin/com/ilya/meetmapkmp/SocialMap/ViewModel/ChatViewModel.kt
@@ -4,6 +4,8 @@ package com.ilya.meetmapkmp.SocialMap.ViewModel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.ilya.meetmapkmp.SocialMap.DATAServices.Chat_Service.ChatWebSocketService
+import com.ilya.meetmapkmp.SocialMap.DataModel.DeleteMessage
+import com.ilya.meetmapkmp.SocialMap.DataModel.DeleteMessageContent
 
 import com.ilya.meetmapkmp.SocialMap.DataModel.Messageformat
 import com.ilya.meetmapkmp.SocialMap.DataModel.Messages_Chat
@@ -15,12 +17,20 @@ import kotlinx.serialization.Serializable
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 
+// ViewModel контролирующая чат, а также соединение с ним получение собщений
 class ChatViewModel : ViewModel() {
 
     private val _messages = MutableStateFlow<List<Messages_Chat>>(emptyList())
+    private val _deletemessages = MutableStateFlow<List<DeleteMessage>>(emptyList())
+
     val messages: StateFlow<List<Messages_Chat>> get() = _messages
+    val deletemessages: StateFlow<List<DeleteMessage>> get() = _deletemessages
+
+
 
     private val chatService = ChatWebSocketService()
+
+
 
     fun connectToChat(roomId: String, uid: String, key: String, name: String) {
         chatService.connectToWebSocket("wss://meetmap.up.railway.app/chat/$roomId?username=Ilya&uid=$uid&key=$key")
@@ -41,6 +51,16 @@ class ChatViewModel : ViewModel() {
 
         // Отправка через WebSocket
         chatService.sendMessage(jsonMessage)
+    }
+
+    fun sendDeleteMessage(deleteMessages : List<String>) {
+        val message = DeleteMessageContent(deleteMessages)
+        // Сериализуем объект Messageformat в строку JSON
+        val jsonMessage = Json.encodeToString(message)
+
+        // Отправка через WebSocket
+        chatService.sendMessage(jsonMessage)
+
     }
 
     fun disconnectFromChat() {
